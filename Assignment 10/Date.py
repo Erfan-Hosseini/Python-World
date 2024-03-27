aa02d55ac0b842bd17d9ffdd100b893bafc05344
Erfan-Hosseini <74896602+Erfan-Hosseini@users.noreply.github.com>
2024-03-27 12:41:41 +0330

    Date.py

---
+++ Assignment 10/Date.py
@@ -0,0 +1,31 @@
+class Date_History:
+    def __init__(self, day, month, year):
+        
+        #properties
+        
+        self.day = day
+        self.month = month
+        self.year = year
+   
+    #methods
+
+    def Solar_to_Lunar(self):
+        ...
+
+    def Lunar_to_Solar(self):
+        ...
+
+    def Solar_to_Gregorian(self):
+        ...
+
+    def Gregorian_to_Solar(self):
+        ...
+
+    def Lunar_to_Gregorian(self):
+        ...
+
+    def Gregorian_to_Lunar(self):
+        ...
+
+    def date_difference(self):
+        ...
